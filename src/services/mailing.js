5f4b2a4718c178aa30c94097aa35d1e6776accbc
SuperClase <amarqueznovella@hotmail.com>
2024-06-25 20:25:23 +0200

    Add support mail: route, router, controller

--- src/services/mailing.js
+++ src/services/mailing.js
@@ -176,3 +176,80 @@ export const createChangePassword = (password) => {
   </body>
   </html>`;
 };
+
+export async function sendSupportMail(email, subject, message) {
+  // TODO: actualizar correo de soporte
+
+  // TODO: es correcto enviar copia al remitente?
+
+  // TODO: no está cogiendo bien los estilos
+
+  return await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: 'eduplat.bienesdar@gmail.com',
+    cc: email,
+    subject: `Mensaje de soporte: ${subject} - ${email}`,
+    html: createSupportMessage(email, subject, message),
+  });
+}
+
+export const createSupportMessage = (email, subject, message) => {
+  return `<!DOCTYPE html>
+  <html lang="es">
+  <style>
+  html {
+    background-color: #ffffff;
+  }
+
+  body {
+    max-width: 600px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    margin: auto;
+    background-color: rgb(229, 255, 246);
+    padding: 40px;
+    border-radius: 4px;
+    margin-top: 10px;
+  }
+
+  h1 {
+    color: #17415f;
+    margin-bottom: 20px;
+  }
+
+  p {
+    margin-bottom: 15px;
+    color: #17415f;
+  }
+
+  a {
+    color: #17415f;
+    text-decoration: none;
+    font-style: italic;
+    font-weight: bold;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  strong {
+    color: #17415f;
+  }
+
+  .firma {
+    font-weight: bold;
+    color: #ff8c00;
+  }
+  </style>
+    <body>
+      <h1>Nuevo mensaje de soporte recibido de: ${email}</h1>
+      <p class='strong'>Asunto:<p/>
+      <p>${subject}<p/>
+
+      <p class='strong'>Mensaje:<p/>
+      <p>${message}</p>
+      <br/>
+      <p class='firma'>Equipo de Eduplat.</p>
+    </body>
+  </html>`
+};
