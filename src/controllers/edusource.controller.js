481a3edd8d37bf743473e358ef157a4a057e4913
SuperClase <amarqueznovella@hotmail.com>
2024-07-02 17:01:39 +0200

    creadas rutas y esquema principales funciones

---
+++ src/controllers/edusource.controller.js
@@ -0,0 +1,25 @@
+
+// TODO: añadir funcion 
+export const getEdusourceById = async (req, res) => {
+    res.send('getEdusourceById')
+}
+
+// TODO: añadir funcion 
+export const getEdusources = async (req, res) => {
+    res.send('getEdusources')
+}
+
+// TODO: añadir funcion 
+export const newEdusource = async (req, res) => {
+    res.send('newEdusource')
+}
+
+// TODO: añadir funcion 
+export const editEdusource = async (req, res) => {
+    res.send('editEdusource')
+}
+
+// TODO: añadir funcion 
+export const deleteEdusource = async (req, res) => {
+    res.send('deleteEdusource')
+}
