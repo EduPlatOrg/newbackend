9f00706ff51b05a82ea2028ba535af4aa5894505
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-08-15 17:11:55 +0200

    add timestamp to inscription model

--- src/models/inscription.model.js
+++ src/models/inscription.model.js
@@ -1,32 +1,37 @@
 import mongoose from 'mongoose';
-const inscriptionScheme = mongoose.Schema({
-  eventId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'Event',
+const inscriptionScheme = mongoose.Schema(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Event',
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    description: {
+      type: String,
+      maxLenght: 500,
+    },
+    inPersonApplication: {
+      type: Boolean,
+    },
+    premiumApplication: {
+      type: Boolean,
+    },
+    shareResources: {
+      type: Boolean,
+    },
+    proccessed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-  description: {
-    type: String,
-    maxLenght: 500,
-  },
-  inPersonApplication: {
-    type: Boolean,
-  },
-  premiumApplication: {
-    type: Boolean,
-  },
-  shareResources: {
-    type: Boolean,
-  },
-  proccessed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model('Inscription', inscriptionScheme);
